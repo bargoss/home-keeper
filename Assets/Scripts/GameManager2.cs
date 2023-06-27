97eda67e0944749eee77e3aea09206e8a5e07863
bargoss <yesiltepebaran98@gmail.com>
2023-06-27 15:21:28 +0300

    scene loaded into custom world (but its slow)

--- Assets/Scripts/GameManager2.cs
+++ Assets/Scripts/GameManager2.cs
@@ -17,77 +17,58 @@ namespace DefaultNamespace
         //private SystemHandle m_SimulationSystemGroup;
         //private SystemHandle m_PresentationSystemGroup;
         //private SystemHandle m_InitializationSystemGroup;
-        private int m_Frames = 0;
-        
+        public static Entity SubSceneEntity;
+
         //public SubScene SubScene;
         //private Entity m_SceneEntity;
 
 
 
-        private World CreateWorld2(string name)
+        private World GetDefaultWorld()
+        {
+            return World.DefaultGameObjectInjectionWorld;
+        }
+        private World CreateWorld(string name)
         {
             var world = new World(name);
+            
             // add default system groups
-            world.GetOrCreateSystem<InitializationSystemGroup>();
+            var initializationSystemGroup = world.GetOrCreateSystem<InitializationSystemGroup>();
             world.GetOrCreateSystem<SimulationSystemGroup>();
             world.GetOrCreateSystem<PresentationSystemGroup>();
 
             var systems = DefaultWorldInitialization.GetAllSystems(WorldSystemFilterFlags.Default);
             DefaultWorldInitialization.AddSystemsToRootLevelSystemGroups(world, systems);
-
-            return world;
-        }
-        private World CreateWorld(string name)
-        {
-            var world = BaransWorldInitialization.Initialize(name);
-            // get the SceneEntity of the subscene with name "New Scene"
-            //Entity sceneEntity = world.EntityManager.CreateEntity();
-            //world.EntityManager.AddComponentData(sceneEntity, new SceneReference {SceneGUID = new GUID("New Sub Scene")});
+            
+            initializationSystemGroup.Update(world.Unmanaged);
             
             return world;
         }
         private void LoadScene(){
-            //m_SceneEntity = m_World.EntityManager.CreateEntity();
-            //m_World.EntityManager.AddComponentData(m_SceneEntity, new SceneReference {SceneGUID = SubScene.SceneGUID});
-            //SceneSystem.LoadSceneAsync(m_World.Unmanaged,SubScene.SceneGUID, new SceneSystem.LoadParameters() { Flags = SceneLoadFlags.LoadAdditive });
-            //SceneSystem.LoadSceneAsync(m_World.Unmanaged, m_SceneEntity, new SceneSystem.LoadParameters() { Flags = SceneLoadFlags.LoadAdditive });
-            
-            
-            
             const string subScenePath = "Assets/Scenes/SampleScene/New Sub Scene.unity";
             var guid = AssetDatabase.GUIDFromAssetPath(subScenePath);
-            var subSceneEntity = SceneSystem.LoadSceneAsync(m_World.Unmanaged, guid,
+            SubSceneEntity = SceneSystem.LoadSceneAsync(m_World.Unmanaged, guid,
                 new SceneSystem.LoadParameters() { Flags = SceneLoadFlags.LoadAdditive });
         }
 
 
-        private void InitializeMyStuff()
-        {
-            var prefabsComponent = m_World.EntityManager
-                .CreateEntityQuery(typeof(MyEntityPrefabsComponent))
-                .GetSingleton<MyEntityPrefabsComponent>();
-            
-            var myPrefab = prefabsComponent.E0;
-            
-            // instantiate the prefab
-            var prefab = m_World.EntityManager.Instantiate(myPrefab);
-            // set its position
-            m_World.EntityManager.SetComponentData(prefab, new LocalTransform()
-            {
-                Position = new float3(10, 0, 0),
-                Rotation = quaternion.identity,
-                Scale = 2,
-            });
-            
-            //CreateTestEntity();
-        }
         private void Start()
         {
-            //m_World = CreateWorld("barans world");
-            m_World = CreateWorld2("barans world");
+            m_World = CreateWorld("bargos world");
+            //m_World = GetDefaultWorld();
+            
+            DebugSceneLoadStatus();
+            
             LoadScene();
+            
+            //const string subScenePath = "Assets/Scenes/SampleScene/New Sub Scene.unity";
+            //var guid = AssetDatabase.GUIDFromAssetPath(subScenePath);
+            //SubSceneEntity = SceneSystem.LoadSceneAsync(m_World.Unmanaged, guid,
+            //    new SceneSystem.LoadParameters() { Flags = SceneLoadFlags.LoadAdditive });
+            
         }
 
+          /*      
         private Entity CreateTestEntity()
         {
             var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
@@ -108,29 +89,58 @@ namespace DefaultNamespace
             commandBuffer.Playback(m_World.EntityManager);
             return entity;
         }
+*/
 
-
+        /*
+        private void InitializeMyStuff()
+        {
+            var prefabsComponent = m_World.EntityManager
+                .CreateEntityQuery(typeof(MyEntityPrefabsComponent))
+                .GetSingleton<MyEntityPrefabsComponent>();
+            
+            var myPrefab = prefabsComponent.E0;
+            
+            // instantiate the prefab
+            var prefab = m_World.EntityManager.Instantiate(myPrefab);
+            // set its position
+            m_World.EntityManager.SetComponentData(prefab, new LocalTransform()
+            {
+                Position = new float3(10, 0, 0),
+                Rotation = quaternion.identity,
+                Scale = 2,
+            });
+            
+            //CreateTestEntity();
+        }
+        */
+        
         private void FixedUpdate()
         {
-            //var sceneLoaded = SceneSystem.IsSceneLoaded(m_World.Unmanaged, m_SceneEntity);
-            //Debug.Log("scene loaded: " + sceneLoaded);
-            //if (sceneLoaded)
-            if (true)
-            {
-                if (m_Frames == 0)
-                {
-                    m_World.GetExistingSystem<InitializationSystemGroup>().Update(m_World.Unmanaged);
-                    //InitializeMyStuff();
-                }
-                
-                m_World.GetExistingSystem<SimulationSystemGroup>().Update(m_World.Unmanaged);
-                m_Frames++;
-            }
+            DebugSceneLoadStatus();
+
+            
+            m_World.GetExistingSystem<InitializationSystemGroup>().Update(m_World.Unmanaged);
+            m_World.GetExistingSystem<SimulationSystemGroup>().Update(m_World.Unmanaged);
         }
 
         private void LateUpdate()
         {
-            m_World.GetExistingSystem<PresentationSystemGroup>().Update(m_World.Unmanaged);
+            m_World.GetExistingSystem<PresentationSystemGroup>().Update(m_World.Unmanaged);        
+        }
+
+        private void DebugSceneLoadStatus()
+        {
+            //var sceneLoadStatus = SceneSystem.GetSceneStreamingState(m_World.Unmanaged, SubSceneEntity);
+            var sceneLoadStatus = SceneSystem.GetSceneStreamingState(m_World.Unmanaged, SubSceneEntity);
+            Debug.Log("scene loading status: " + sceneLoadStatus);
+
+            /*
+            if (Input.GetKey(KeyCode.D))
+            {
+                var sceneLoadedBool = SceneSystem.IsSceneLoaded(World.DefaultGameObjectInjectionWorld.Unmanaged, SubSceneEntity);
+                Debug.Log("scene loaded bool: " + sceneLoadedBool);
+            }
+            */
         }
 
         private void Noted()
