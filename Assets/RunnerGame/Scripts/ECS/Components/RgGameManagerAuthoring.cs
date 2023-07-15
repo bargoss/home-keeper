ea3ae06945a7e44ae876de246cc5b74a7fdff775
bargoss <yesiltepebaran98@gmail.com>
2023-07-15 20:08:15 +0300

    player movement

---
+++ Assets/RunnerGame/Scripts/ECS/Components/RgGameManagerAuthoring.cs
@@ -0,0 +1,53 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace RunnerGame.Scripts.ECS.Components
+{
+    public class RgGameManagerAuthoring : MonoBehaviour
+    {
+        // constants
+        public float PlayerForwardSpeed = 1;
+        public float PlayerSidewaysP = 1;
+        public float PlayerSidewaysD = 0.1f;
+        public Transform MainCamera;
+        public float RoadWidth = 10;
+        
+        // prefabs
+        public GameObject PlayerPrefab;
+        public GameObject ParticlePrefab;
+        
+        
+        public class RgGameManagerAuthoringBaker : Baker<RgGameManagerAuthoring>
+        {
+            public override void Bake(RgGameManagerAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponentObject(entity,
+                    new RgGameManager
+                    {
+                        PlayerForwardSpeed = authoring.PlayerForwardSpeed,
+                        PlayerSidewaysP = authoring.PlayerSidewaysP,
+                        PlayerSidewaysD = authoring.PlayerSidewaysD,
+                        MainCamera = authoring.MainCamera,
+                        PlayerPrefab = GetEntity(authoring.PlayerPrefab, TransformUsageFlags.Dynamic),
+                        ParticlePrefab = GetEntity(authoring.ParticlePrefab, TransformUsageFlags.Dynamic),
+                        RoadWidth = authoring.RoadWidth,
+                    }
+                );
+            }
+        }
+    }
+
+    public class RgGameManager : IComponentData
+    {
+        public float PlayerForwardSpeed;
+        public float PlayerSidewaysP;
+        public float PlayerSidewaysD;
+        public Transform MainCamera;
+        public Entity PlayerPrefab;
+        public Entity ParticlePrefab;
+
+        public float RoadWidth;
+
+    }
+}
---
+++ Assets/RunnerGame/Scripts/ECS/Systems/GameManagerSystem.cs
@@ -0,0 +1,68 @@
+﻿using DefaultNamespace;
+using RunnerGame.Scripts.ECS.Components;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace RunnerGame.Scripts.ECS.Systems
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    public partial class GameManagerSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            if (!SystemAPI.ManagedAPI.TryGetSingleton<RgGameManager>(out var gameManager))
+            {
+                return;
+            }
+            var camera = gameManager.MainCamera;
+            
+            var playerHorizontalInput = Input.GetAxis("Horizontal");
+            
+            // move player and camera
+            Entities.ForEach((Entity entity, ref PhysicsVelocity physicsVelocity, ref LocalTransform localTransform, in RgPlayer rgPlayer) =>
+            {
+                var velocity = physicsVelocity.Linear;
+                var angularVelocity = physicsVelocity.Angular;
+
+                var targetVelocity = Utility.Forward * gameManager.PlayerForwardSpeed + Utility.Right * playerHorizontalInput * gameManager.PlayerSidewaysP;
+                velocity = math.lerp(velocity, targetVelocity, SystemAPI.Time.DeltaTime * 2);
+                velocity.y = 0;
+                
+                // remain inside the road bounds
+                var pos = localTransform.Position;
+                pos.y = 0;
+                if(pos.x > gameManager.RoadWidth * 0.5f)
+                {
+                    pos.x = gameManager.RoadWidth * 0.5f;
+                    if (velocity.x > 0)
+                    {
+                        velocity.x = 0;
+                    }
+                }
+                else if(pos.x < -gameManager.RoadWidth * 0.5f)
+                {
+                    pos.x = -gameManager.RoadWidth * 0.5f;
+                    if (velocity.x < 0)
+                    {
+                        velocity.x = 0;
+                    }
+                }
+                localTransform.Position = pos;
+
+                angularVelocity = 0;
+
+                physicsVelocity.Linear = velocity;
+                physicsVelocity.Angular = angularVelocity;
+                localTransform.Rotation = quaternion.LookRotation(Utility.Forward, Utility.Up);
+                
+
+                camera.position = (Vector3)localTransform.Position + new Vector3(0, 8, -15);
+                camera.LookAt(localTransform.Position);
+            }).WithoutBurst().Run();
+        }
+    }
+}
