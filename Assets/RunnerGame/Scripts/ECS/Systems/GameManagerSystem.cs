ff86cc603c7d5688d06f77b10a6008604e584b87
bargoss <yesiltepebaran98@gmail.com>
2023-07-21 23:13:03 +0300

    .

--- Assets/RunnerGame/Scripts/ECS/Systems/GameManagerSystem.cs
+++ Assets/RunnerGame/Scripts/ECS/Systems/GameManagerSystem.cs
@@ -15,6 +15,7 @@ namespace RunnerGame.Scripts.ECS.Systems
     public partial class GameManagerSystem : SystemBase
     {
         private int m_SpawnedCount = 0;
+        private float3 m_SmoothDirection = new float3(0, 0, 1);
         protected override void OnUpdate()
         {
             if (!SystemAPI.TryGetSingleton<RgGameManagerData>(out var gameManager))
@@ -33,20 +34,22 @@ namespace RunnerGame.Scripts.ECS.Systems
             // move player and camera
             Entities.ForEach((Entity entity, ref PhysicsVelocity physicsVelocity, ref LocalTransform localTransform, in RgPlayer rgPlayer) =>
             {
-                localTransform = HandlePlayerMovement(ref physicsVelocity, gameManager, playerHorizontalInput, ref localTransform, World.Time.DeltaTime);
+                localTransform = HandlePlayerMovement(ref physicsVelocity, gameManager, playerHorizontalInput, ref localTransform, World.Time.DeltaTime, ref m_SmoothDirection);
+                
 
 
                 if (camera != null)
                 {
-                    camera.transform.position = (Vector3)localTransform.Position + new Vector3(0, 10, -15) * 0.6f;
-                    camera.transform.LookAt((Vector3)localTransform.Position + Vector3.forward * 4);
+                    camera.transform.position = (Vector3)localTransform.Position + new Vector3(0, 20, -10) * 1f;
+                    camera.transform.LookAt((Vector3)localTransform.Position + Vector3.forward * 1);
                 }
 
 
+                var random = Random.CreateFromIndex((uint)(SystemAPI.Time.ElapsedTime * 321321));
                 // handle spawning of particles:
-                if (Input.GetKey(KeyCode.Space) || m_SpawnedCount < 1000) // 2000
+                if (Input.GetKey(KeyCode.Space) || m_SpawnedCount < 1000 && random.NextFloat() % 1f >0.0f) // 2000
                 {
-                    var spawnPosition = localTransform.Position + Utility.Forward * 16 + Utility.Up * 3;
+                    var spawnPosition = localTransform.Position.z * new float3(0,0,1) + Utility.Forward * 16 + Utility.Up * 3 + new float3(4,0,0) * math.sin((float)SystemAPI.Time.ElapsedTime * 0.3f);
                     var randomness = Random.CreateFromIndex((uint)(localTransform.Position.z * 1000))
                         .NextFloat3Direction() * 0.0001f; //* 3.25f;
 
@@ -61,9 +64,9 @@ namespace RunnerGame.Scripts.ECS.Systems
             ecb.Playback(EntityManager);
             ecb.Dispose();
         }
-
+        
         private static LocalTransform HandlePlayerMovement(ref PhysicsVelocity physicsVelocity, RgGameManagerData gameManagerData,
-            float playerHorizontalInput, ref LocalTransform localTransform, float deltaTime)
+            float playerHorizontalInput, ref LocalTransform localTransform, float deltaTime, ref float3 smoothDirection)
         {
             var velocity = physicsVelocity.Linear;
             var angularVelocity = physicsVelocity.Angular;
@@ -99,7 +102,12 @@ namespace RunnerGame.Scripts.ECS.Systems
 
             physicsVelocity.Linear = velocity;
             physicsVelocity.Angular = angularVelocity;
-            localTransform.Rotation = quaternion.LookRotation(Utility.Forward, Utility.Up);
+            
+            //var targetRotation = quaternion.LookRotation( math.normalizesafe(velocity, new float3(0,0,1)), Utility.Up);
+            //var lerpedRotation = math.slerp(localTransform.Rotation, targetRotation, deltaTime * 5);
+            //localTransform.Rotation = lerpedRotation;
+            
+            localTransform.Rotation = quaternion.identity;;
             return localTransform;
         }
     }
