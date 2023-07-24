505f73bd7054918b7b580876da15dcf15f0301f3
bargoss <yesiltepebaran98@gmail.com>
2023-07-24 19:42:12 +0300

    wip

--- Assets/DefenderGame/Scripts/Systems/DeGameManager.cs
+++ Assets/DefenderGame/Scripts/Systems/DeGameManager.cs
@@ -5,12 +5,28 @@ namespace DefenderGame.Scripts.Systems
 {
     public partial class DeGameManager : SystemBase
     {
+        // this class is the entry point object for the game
+        
         public event System.Action GameFinished;
         
-        
+        protected override void OnCreate()
+        {
+            RequireForUpdate<DeGameManager>();
+        }
+
         protected override void OnUpdate()
         {
             
         }
+
+        public void CleanUp()
+        {
+            
+        }
+
+        //public void LoadLevel(. . .)
+        //{
+        //    
+        //}
     }
 }
