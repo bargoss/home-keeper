473327c5a7f6e235dae5aeb115f24c5d35de6c13
bargoss <yesiltepebaran98@gmail.com>
2023-09-23 19:10:53 +0300

    strategy game experiment?

---
+++ Assets/_StrategyGame/Scripts/Components/StActor.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.NetCode;
+
+namespace _StrategyGame.Scripts.Components
+{
+    public struct StUnit : IComponentData
+    {
+        [GhostField] public float2 TargetPosition;
+    }
+    public struct StSelectable : IComponentData
+    {
+        
+    }
+    public struct StSelected : IComponentData
+    {
+        
+    }
+}
